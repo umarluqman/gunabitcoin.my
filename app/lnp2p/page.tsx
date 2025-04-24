05b65f551c9d0d39150a7616f08e89be6bc74d0b
umarluqman <umarluqman.78@gmail.com>
2025-04-24 09:38:02 +0800

    push

---
+++ app/lnp2p/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+
+export default function MyApp() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "lnp2p-malaysia-step-by-step" });
+      cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
+    })();
+  }, []);
+  return (
+    <div className="mt-20">
+      <Cal
+        namespace="lnp2p-malaysia-step-by-step"
+        calLink="gunabitcoin.my/lnp2p-malaysia-step-by-step"
+        style={{ width: "100%", height: "100%", overflow: "scroll" }}
+        config={{ layout: "month_view" }}
+      />
+    </div>
+  );
+}
