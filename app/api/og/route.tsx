6f30aba7f7a9aa6beb27ea65eea095a6b191e1ec
umarluqman <umarluqman.78@gmail.com>
2025-05-04 23:51:36 +0800

    remove jolly blog

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -25,7 +25,7 @@ export async function GET(req: NextRequest) {
     return new ImageResponse(
       (
         <div tw="flex relative flex-col p-12 w-full h-full items-start text-black bg-white">
-          <div tw="flex items-center">
+          {/* <div tw="flex items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -39,8 +39,8 @@ export async function GET(req: NextRequest) {
               <path d="M4 4a16 16 0 0 1 16 16" />
               <circle cx="5" cy="19" r="1" />
             </svg>
-            <p tw="ml-2 font-bold text-2xl">JollyBlog</p>
-          </div>
+            <p tw="ml-2 font-bold text-2xl">gunabitcoin</p>
+          </div> */}
           <div tw="flex flex-col flex-1 py-10">
             <div tw="flex text-xl uppercase font-bold tracking-tight font-normal">
               BLOG POST
